1533d948c237ccc696c61b5236a0507002bbdee3
metallimiach <metallimiach@gmail.com>
2016-02-27 23:25:46 +0300

    Пояснительная записка

---
+++ AllBanksDiploma/AllBanksDiploma/Controllers/ContentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllBanksDiploma.Controllers
+{
+    public class ContentController : Controller
+    {
+        public ActionResult Banks()
+        {
+            return View();
+        }
+        public ActionResult Deposits()
+        {
+            return View();
+        }
+        public ActionResult Loans()
+        {
+            return View();
+        }
+        public ActionResult Currency()
+        {
+            return View();
+        }
+    }
+}
