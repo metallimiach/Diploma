353f84dd3374e5fd0c1831ce8d0a4736ff0e4b0f
metallimiach <metallimiach@gmail.com>
2016-02-28 16:56:48 +0300

    Пояснительная записка

---
+++ AllBanksDiploma/AllBanksDiploma/Models/Bank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AllBanksDiploma.Models
+{
+    public class Bank
+    {
+        public int BankId { get; set; } //id Банка
+        public string BankName { get; set; } //Название
+        public string Description { get; set; } //Описание
+        public string WebPage { set; get; } //Вэб-страница
+        public byte[] ImageData { set; get; } // Изображение
+        public string ImageMimeType { set; get; } // Mime тип изображения
+        public virtual ICollection<Branch> Branches { get; set; }
+        public virtual ICollection<Deposit> Deposits { get; set; }
+        public virtual ICollection<Loan> Loans { get; set; }
+    }
+}
